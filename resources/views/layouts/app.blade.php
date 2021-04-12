3debb03940e9a00fc696b8ddf16e5a6c87d155e8
Mnabawy <m_ninja61@yahoo.com>
2021-04-12 19:14:41 +0200

    authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,34 +9,43 @@
 
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
 </head>
+
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-2">
         <ul class="flex items-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href={{ route('dashboard') }} class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Posts</a>
             </li>
         </ul>
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Mohamed Nabawy</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Mohamed Nabawy</a>
+                </li>
+                <li>
+                    <form action={{ route('logout') }} method="POST" class="inline p-3">
+                        @csrf
+                        <button>Logout</button>
+                    </form>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href={{ route('login') }} class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href={{ route('register') }} class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
 </body>
-</html> 
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Authh\LoginController;
+use App\Http\Controllers\Authh\LogoutController;
+use App\Http\Controllers\Authh\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +16,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+->name('dashboard');
+
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/posts', function () {
     return view('posts.index');
