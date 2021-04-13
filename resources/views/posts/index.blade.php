79247b01d627bd1275dd357872a8f69121896b2b
Mnabawy <m_ninja61@yahoo.com>
2021-04-13 09:22:24 +0200

    posts component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,38 @@
 
 @section('content')
     <div class="flex justify-center">
-        
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            posts
+            <form action={{ route('posts') }} method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="body">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="4"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                        placeholder="Post Something!"></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                </div>
+            </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold mr-3">{{$post->user->name}}</a><span class="text-grey-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>there are not posts here</p>
+            @endif
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Authh\LoginController;
 use App\Http\Controllers\Authh\LogoutController;
 use App\Http\Controllers\Authh\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,16 +23,15 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
-->name('dashboard');
+    ->name('dashboard');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'store']);
-
-Route::get('/posts', function () {
-    return view('posts.index');
-})->name('posts');
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
